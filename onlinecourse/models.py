750d4677eadb03b1ec5fad4a66073504bbe972c3
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-11-25 14:13:11 -0500

    Added a submission model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -115,3 +115,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
